91bd340998e062a913a70597485a2dfbd2b9fbfe
jgehrmann <mail@jangehrmann.de>
2017-07-18 11:31:57 +0200

    Updated documentation. And suppressed inspectation for parameter injection.

--- de.continous.rest.js
+++ de.continous.rest.js
@@ -47,31 +47,34 @@ var REST = (function () {
         properties.debug = false;
     };
 
+    // noinspection JSValidateJSDoc
     /**
      * Sends a get request to consume data from a Restful service.
      *
      * @param url {string} Url where the resource should be found.
      * @param asynch {boolean} Is this request actually synchronous or asynchronous?
-     * @param onSuccess {function(responseText,status,successful)} function executes on a successful request.
+     * @param onSuccess {function(responseText,statusCode,statusMessage)} function executes on a successful request.
      * @constructor
      */
     REST.prototype.GET = function (url, asynch, onSuccess) {
         request(methods.get, url, asynch, onSuccess, null, null);
     };
 
+    // noinspection JSValidateJSDoc
     /**
      * Creates new resource on the server. ResponseText provides a link to the newly created resource on the Restful service.
      *
      * @param url {string} Url where the new resource should be created.
      * @param asynch {boolean} Is this request actually synchronous or asynchronous?
      * @param body The body which should be transfered to the Restful service.
-     * @param onSuccess {function(responseText,status,successful)} function executes on a successful request.
+     * @param onSuccess {function(responseText,statusCode,statusMessage)} function executes on a successful request.
      * @constructor
      */
     REST.prototype.POST = function (url, asynch, body, onSuccess) {
         request(methods.post, url, asynch, onSuccess, body, null);
     };
 
+    // noinspection JSValidateJSDoc
     /**
      * PUT updates a existing resource. If the resource doesn't exist it tries to create it.
      * When it doesn't exist, it will return a link to the newly created resource.
@@ -79,37 +82,40 @@ var REST = (function () {
      * @param url {string} The url to the resource beeing updated.
      * @param asynch {boolean} Is this request actually synchronous or asynchronous?
      * @param body The body which should be transfered to the Restful service.
-     * @param onSuccess {function(responseText,status,successful)} function executes on a successful request.
+     * @param onSuccess {function(responseText,statusCode,statusMessage)} function executes on a successful request.
      * @constructor
      */
     REST.prototype.PUT = function (url, asynch, body, onSuccess) {
         request(methods.put, url, asynch, onSuccess, body, null);
     };
 
+    // noinspection JSValidateJSDoc
     /**
      * DELETE deletes a resource at the given url.
      *
      * @param url {string} Url where the resource should be deleted.
      * @param asynch {boolean} Is this request actually synchronous or asynchronous?
-     * @param onSuccess {function(responseText,status,successful)} function executes on a successful request.
+     * @param onSuccess {function(responseText,statusCode,statusMessage)} function executes on a successful request.
      * @constructor
      */
     REST.prototype.DELETE = function (url, asynch, onSuccess) {
         request(methods.delete, url, asynch, onSuccess, null, null);
     };
 
+    // noinspection JSValidateJSDoc
     /**
      * Collects all metadata of a resource.
      *
      * @param url {string} Url where the resource can be found.
      * @param asynch {boolean} Is this request actually synchronous or asynchronous?
-     * @param onSuccess {function(responseText,status,successful)} function executes on a successful request.
+     * @param onSuccess {function(responseText,statusCode,statusMessage)} function executes on a successful request.
      * @constructor
      */
     REST.prototype.HEAD = function (url, asynch, onSuccess) {
         request(methods.head, url, asynch, null, null, onSuccess);
     };
 
+    // noinspection JSValidateJSDoc
     /**
      * Sends the request for all different types of methods.
      * @param method
@@ -117,7 +123,7 @@ var REST = (function () {
      * @param asynchmode
      * @param onReady {function(responseText)}
      * @param body The body of the request which will be send with it. Can be: ArrayBuffer, ArrayBufferView, Blob, Document,DOMString,FormData, string or empty.
-     * @param headerCallback {function(AllHeaders,status,successful)}
+     * @param headerCallback {function(resonseHeader,statusCode,statusMessage)}
      * @return {request}
      */
     function request(method, url, asynchmode, onReady, body, headerCallback) {
@@ -133,12 +139,15 @@ var REST = (function () {
             if (request.readyState === 4 && request.status >= 200 && request.status <= 299) {//successfully requested.
 
                 if (onReady !== null) {
+                    //injecting parameters for later use in callback function
+                    // noinspection JSCheckFunctionSignatures
                     onReady(request.responseText, request.status, message.successful);
                 }
 
                 if (headerCallback !== null) {
-
-                    headerCallback(request.getAllResponseHeaders(), request.status, message.successful); //injecting parameters for later use in callback function
+                    //injecting parameters for later use in callback function
+                    // noinspection JSCheckFunctionSignatures
+                    headerCallback(request.getAllResponseHeaders(), request.status, message.successful);
                 }
             }
         };
